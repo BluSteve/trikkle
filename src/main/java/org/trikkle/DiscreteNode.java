b528eb80104be9296b21ad44103fdec9e5c75a32
BluSteve <steve.scyr@gmail.com>
2024-01-29 20:27:07 +0800

    Offload null check to StrictConcurrentHashMap

--- src/main/java/org/trikkle/DiscreteNode.java
+++ src/main/java/org/trikkle/DiscreteNode.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class DiscreteNode extends Node {
 	private final Map<String, Boolean> isDatumDone = new HashMap<>();
+
 	public DiscreteNode(Set<String> datumNames) {
 		super(datumNames);
 		for (String datumName : datumNames) {
@@ -16,18 +17,12 @@ public class DiscreteNode extends Node {
 	@Override
 	protected void uncheckedAddDatum(String datumName, Object datum) {
 		Map<String, Object> cache = overseer.getCache();
-		if (cache.containsKey(datumName)) {
-			// todo test and remove this
-			throw new IllegalStateException("Datum already present in cache!");
-		}
-		else {
-			cache.put(datumName, datum);
-			isDatumDone.put(datumName, true);
+		cache.put(datumName, datum);
+		isDatumDone.put(datumName, true);
 
-			if (!isDatumDone.containsValue(false)) { // all datums filled
-				setProgress(1);
-				overseer.ticktock(this);
-			}
+		if (!isDatumDone.containsValue(false)) { // all datums filled
+			setProgress(1);
+			overseer.ticktock(this);
 		}
 	}
 }
