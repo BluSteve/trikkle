a0ffbb7c11507c4fa0e05517b9970e0629adf011
BluSteve <billiegatesftw@gmail.com>
2024-02-10 01:32:05 +0800

    StreamNode still not fixed (i think)

--- src/main/java/org/trikkle/StreamNode.java
+++ src/main/java/org/trikkle/StreamNode.java
@@ -37,13 +37,12 @@ public final class StreamNode extends Node {
 	protected void uncheckedAddDatum(String datumName, Object datum) {
 		((Queue) overseer.getCache().get(datumName)).add(datum);
 		synchronized (LOCK) {
-			if (count == limit) {
-				throw new IllegalStateException("StreamNode is already full!");
-			}
-
 			setUsable(true);
-			count++;
 			if (limit != -1) {
+				if (count == limit) {
+					throw new IllegalStateException("StreamNode is already full!");
+				}
+				count++;
 				setProgress((double) count / limit);
 			}
 		}
